8f817f2ea7dd767e79e7046faa27bf397c38fc03
Puxuan He <puxuanhe@Puxuans-MacBook-Pro.local>
2018-06-01 20:54:40 -0500

    update

--- scripts/createImageInfo.js
+++ scripts/createImageInfo.js
@@ -1,25 +1,39 @@
 const fs = require('fs');
 const sharp = require('sharp');
+const _ = require('lodash');
 
-let files = [];
 const basePath = '../photos';
 const outputPath = '../src/images';
 
 
-fs.readdirSync(basePath).forEach(folder => {
+Promise.all(fs.readdirSync(basePath).map(folder => {
     const filesName = fs.readdirSync(`${basePath}/${folder}`);
-    filesName.forEach((file) => {
+    return Promise.all(filesName.map((file) => {
         const image = `${basePath}/${folder}/${file}`;
-        sharp(image).resize(800).toFile(`${outputPath}/thumbnails/${file}`);
-        fs.createReadStream(image).pipe(fs.createWriteStream(`${outputPath}/photos/${file}`));
+        sharp(image).resize(1280).toFile(`${outputPath}/thumbnails/${folder}/${file}`);
+        fs.createReadStream(image).pipe(fs.createWriteStream(`${outputPath}/photos/${folder}/${file}`));
+        
+        // base64 src
+        return new Promise((resolve) => {
+            sharp(image).resize(150).toFile(`${outputPath}/base64/${folder}/${file}`, () => {
+                const base64 = fs.readFileSync(`${outputPath}/base64/${folder}/${file}`, 'base64');
+                resolve({
+                    photo: `images/photos/${folder}/${file}`,
+                    thumbnail: `images/thumbnails/${folder}/${file}`,
+                    base64,
+                });
+            });
+        })
+        
+    }));
+})).then((files) => {
+    fs.writeFile("../src/data/files.json", JSON.stringify(_.flatten(files)), 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
     });
-    files = files.concat(filesName.map(file => `images/photos/${file}`));
-});
-
-fs.writeFile("../src/data/files.json", JSON.stringify(files), 'utf8', function (err) {
-    if (err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
+}, (error) => {
+    console.log(error);
 });
--- src/js/photos.js
+++ src/js/photos.js
@@ -1,10 +1,12 @@
+import 'intersection-observer';
 import './modal';
 
-var imageNodes = document.querySelectorAll('.image-link');
+var imageNodes = document.querySelectorAll('.image-img');
 var modalContentNode = document.getElementById('modal-content');
 var modalNode = document.getElementById('modal');
+var imageLink = document.querySelectorAll('.image-link');
 
-for (let node of imageNodes) {
+imageLink.forEach((node) => {
     node.addEventListener('click', function (e) {
         e.preventDefault();
 
@@ -12,4 +14,21 @@ for (let node of imageNodes) {
         var src = node.getAttribute('href');
         modalContentNode.innerHTML = '<img src="' + src + '"></img>';
     });
-}
+});
+
+var observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            observer.unobserve(entry.target);
+            
+            var target = entry.target;
+            target.src = target.getAttribute('data-src');
+        }
+      });
+}, {
+    rootMargin: '0px 0px',
+});
+
+imageNodes.forEach((node) => {
+    observer.observe(node);
+});
